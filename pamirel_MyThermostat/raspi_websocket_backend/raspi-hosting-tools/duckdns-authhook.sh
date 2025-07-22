4698d91323afdf02059a93be4d9f4c4a7335cc5c
rubi <rubi.zelenik@gmail.com>
2025-07-22 15:41:42 +0200

    Update duckdns-authhook.sh -- token to be inserted

--- pamirel_MyThermostat/raspi_websocket_backend/raspi-hosting-tools/duckdns-authhook.sh
+++ pamirel_MyThermostat/raspi_websocket_backend/raspi-hosting-tools/duckdns-authhook.sh
@@ -2,7 +2,7 @@
 # Used to automatically renew certificate for raspiwebsocket.duckdns.org with certbot --manual-auth-hook flag
 #source: https://github.com/AlwindB/LetsEncrypt-DuckDNS-update
 
-TOKEN="b000e0b2-bb35-4384-8a88-0398a34bb9d0"
+TOKEN="xxx"
 DOMAIN="raspiwebsocket"
 
 echo url="https://www.duckdns.org/update?domains=$DOMAIN&token=$TOKEN&txt=$CERTBOT_VALIDATION&verbose=true" | curl -k -o /rubi/pamirel/hosting_tools/duckdns-authhook.sh -K -
